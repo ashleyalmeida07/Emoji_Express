f40190867dc8fe8c97e98859545545ecc6fcfc1c
Ashley Almeida <155456903+ashleyalmeida07@users.noreply.github.com>
2025-11-11 19:08:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -159,5 +159,8 @@ def claim_reward():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
 
